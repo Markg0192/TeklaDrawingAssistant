aa7a47d86072b7288162dec7ce26e779557ada1e
Markg0192 <markg0192@hotmail.com>
2026-09-15 19:46:06 +0100

    Add adaptive dimension offset settings

--- src/TeklaDrawingAssistant/Models/DimensioningOptions.cs
+++ src/TeklaDrawingAssistant/Models/DimensioningOptions.cs
@@ -2,7 +2,9 @@ namespace TeklaDrawingAssistant.Models
 {
     public sealed class DimensioningOptions
     {
-        public double DimensionOffset { get; set; } = 12.0;
+        public double DimensionOffset { get; set; } = 25.0;
+        public double DimensionOffsetScale { get; set; } = 0.15;
+        public double MaximumDimensionOffset { get; set; } = 100.0;
         public double CoordinateTolerance { get; set; } = 0.5;
         public bool DeleteExistingStraightDimensions { get; set; }
         public bool SaveDrawingAfterRun { get; set; } = true;
